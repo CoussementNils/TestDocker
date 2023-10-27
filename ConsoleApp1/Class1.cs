7187cfb5591cc26e5a761dfaddf93154007f22ea
nils.coussement@gmail.com <nils.coussement@gmail.com>
2023-10-27 17:54:25 +0200

    .editorconfig file added and passed

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -8,9 +8,21 @@ namespace ConsoleApp1
 {
     internal class Class1
     {
-        public string _getSDsqddsq(string INputStTRED)
+        //public string _getSDsqddsq(string INputStTRED)
+        //{
+        //    var a = _getSDsqddsq();
+
+        //    var b = qsd()
+
+        //    return "sqsqddq";
+        //}
+
+        public static string GetStringA(string INputStTRED)
         {
-            return "sqsqddq";
+            var a = "sdqsdqs";
+            var b = "sdqsdsd";
+
+            return $"{a} {b}";
         }
 
     }
